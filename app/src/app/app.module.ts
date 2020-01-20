c8998584ebd64212582a6f523891ecd42ece865a
bartvanmaarschalkerweerd <bart.vanmaarschalkerweerd@outlook.com>
2020-01-20 23:42:16 +0100

    Added most of the main components.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuilderComponent } from './builder/builder.component';
+import { PreviewComponent } from './preview/preview.component';
+import { StartComponent } from './start/start.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		BuilderComponent,
+		PreviewComponent,
+		StartComponent,
+		HeaderComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
