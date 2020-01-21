e2c5ed7c5821a118bf0dc7543a3920eb1fa2608c
bartvanmaarschalkerweerd <bart.vanmaarschalkerweerd@outlook.com>
2020-01-21 23:31:29 +0100

    restructured the app

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BuilderComponent } from './builder/builder.component';
-import { PreviewComponent } from './preview/preview.component';
-import { StartComponent } from './start/start.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		BuilderComponent,
-		PreviewComponent,
-		StartComponent,
 		HeaderComponent,
 		FooterComponent
 	],
